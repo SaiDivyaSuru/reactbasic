ca10fe033a69f8450f33036f95c415efd166d5b5
divyasuru <surusaidivya@gmail.com>
2024-10-03 11:52:39 +0530

    basics

--- hooksbasic/src/App.js
+++ hooksbasic/src/App.js
@@ -1,15 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import FunctionalComponent from './Component/FunctionalComponent';
-import Hookcounter from './Component/Hookcounter';
+
 import Usestateobj from './Component/Usestateobj';
+import ClassCounterOne from './Component/ClassCounterOne';
+import HookCounterOne from './Component/HookCounterOne';
+import ClassMouseEvent from './Component/ClassMouseEvent';
+import HookMouse from './Component/HookMouse';
 
 function App() {
   return (
     <div className="App">
       {/* <FunctionalComponent/> */}
       {/* <Hookcounter/> */}
-      <Usestateobj/>
+      {/* <Usestateobj/> */}
+       {/* <ClassCounterOne/>  */}
+      {/* <HookCounterOne/> */}
+      {/* <ClassMouseEvent/> */}
+      <HookMouse/>
     </div>
   );
 }
