acae76ccf8756cb46a6b9c0ec86d984bfd84f4ca
divyasuru <surusaidivya@gmail.com>
2024-10-08 14:58:27 +0530

    render

--- hooksbasic/src/App.js
+++ hooksbasic/src/App.js
@@ -13,13 +13,25 @@ import Reducer from './Component/Reducer';
 import ComplexReducer from './Component/ComplexReducer';
 import ClassClickCounter from './Component/ClassClickCounter';
 import HoverCounter from './Component/HoverCounter';
+import ClickCounter2 from './Component/ClickCounter2';
+import HoverCounter2 from './Component/HoverCounter2';
+import User from './Component/User';
+import Counter from './Component/Counter';
 
 
 function App() {
   return (
     <div className="App">
-      <ClassClickCounter name='sai'/>
-      <HoverCounter/>
+      {/* <ClassClickCounter name='sai'/>
+      <HoverCounter/> */}
+      {/* <ClickCounter2/>
+      <HoverCounter2/>
+      <User render={(isLoggedIn) => isLoggedIn?'Vishwas':'Guest'}/> */}
+      <Counter render = {(count,incrementCount) => 
+        <ClickCounter2 count={count} incrementCount={incrementCount}/>} />
+      <Counter render = {(count,incrementCount) => 
+        <HoverCounter2 count={count} incrementCount={incrementCount}/>} />
+
     </div>
   );
 }
